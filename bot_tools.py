7240f256f727ec5947b786512c951ceab522ebdb
Achxy <luminosityzzz00@gmail.com>
2022-01-23 14:25:21 +0530

    bot checks for vacancy before adding emotes

---
+++ bot_tools.py
@@ -0,0 +1,15 @@
+import discord
+
+def static_vacancy(guild: discord.guild.Guild) -> int:
+    """
+    This function takes guild object as sole argument
+    Returns an integer reflecting count of additional static emotes the guild could potentially accept
+    """
+    return guild.emoji_limit - len([_ for _ in guild.emojis if not _.animated])
+
+def animated_vacancy(guild: discord.guild.Guild) -> int:
+    """
+    This function takes guild object as sole argument
+    Returns an integer reflecting count of additional animated emotes the guild could potentially accept
+    """
+    return guild.emoji_limit - len([_ for _ in guild.emojis if _.animated])
--- cogs/add.py
+++ cogs/add.py
@@ -1,18 +1,26 @@
 import discord
 import typing
 from discord.ext import commands
-
+from bot_tools import static_vacancy, animated_vacancy
 
 class add_(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     # TODO: Add more features and error handling to this.
-    # TODO: Check for guild vacancy for additonal emotes
+    # TODO: Reduce the number of function calls by getting the emote count first then decrementing them according to success
     @commands.command()
     @commands.has_permissions(manage_emojis=True)
     async def add(self, ctx, *emojis: typing.Union[discord.PartialEmoji, str]):
         for index, each_emoji in enumerate(emojis):
+
+            if static_vacancy(ctx.guild) == 0 and animated_vacancy(ctx.guild) == 0:
+                # Send a message indicating that the guild cannot accept any more emotes
+                # If this condition is hit then we want to terminate the function definition
+                embed=discord.Embed(title="The guild is absolutely full", description="Your guild is full of emotes, it cannot accept any more of either static or animated emotes, please remove some emotes before trying again")
+                await ctx.send(embed=embed)
+                # End the function definiton
+                return
 
             if isinstance(each_emoji, str):
                 # To not process anything further if the user has given us an non-custom emoji.
@@ -25,6 +33,25 @@ class add_(commands.Cog):
                 )
                 await ctx.send(embed=embed)
                 continue
+            
+            if not each_emoji.animated and static_vacancy(ctx.guild) == 0:
+                # Send a message indicating that the guild cannot accept any more static emotes
+                embed=discord.Embed(title="Guild cannot accept any more of that", description=f"The guild cannot accept any more static emotes (perhaps add some animated emotes now) as such **{each_emoji.name}** was not added to the guild")
+                embed.set_footer(
+                    text=f"{index + 1} of {len(emojis)} to add {'' if not (index + 1) == len(emojis) else '(over)'}"
+                )
+                await ctx.send(embed=embed)
+                continue
+
+            if each_emoji.animated and animated_vacancy(ctx.guild) == 0:
+                # Send a message indicating that the guild cannot accept any more animated emotes
+                embed=discord.Embed(title="Guild cannot accept any more of that", description=f"The guild cannot accept any more animated emotes (perhaps add some static emotes now) as such **{each_emoji.name}** was not added to the guild")
+                embed.set_footer(
+                    text=f"{index + 1} of {len(emojis)} to add {'' if not (index + 1) == len(emojis) else '(over)'}"
+                )
+                await ctx.send(embed=embed)
+                continue
+            
 
             # All working, add the emoji to the guild.
             added_emoji = await ctx.guild.create_custom_emoji(
