582365642b911708870704848084d27cc2a69ffe
Achxy <luminosityzzz00@gmail.com>
2022-01-23 15:17:22 +0530

    removed code snippet that was in a commment

--- bot_tools.py
+++ bot_tools.py
@@ -15,29 +15,6 @@ def animated_vacancy(guild: discord.guild.Guild) -> int:
     return guild.emoji_limit - len([_ for _ in guild.emojis if _.animated])
 
 
-"""
-# Status to Online (The green one)
-await client.change_presence(status=discord.Status.online)
-
-# Status to Idle (The orange one)
-await client.change_presence(status=discord.Status.idle)
-
-# Status to Do not disturb (The red one)
-await client.change_presence(status=discord.Status.dnd)
-
-
-# Playing ____
-await bot.change_presence(activity=discord.Game(name='Minecraft'))
-# Streaming ____
-await bot.change_presence(activity=discord.Streaming(name="Chess", url=twitch_url))
-# Watching ____
-await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Harry Potter"))
-# Competing in ____
-await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name="the House Cup"))
-# Listening to ____
-await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="!help"))
-
-"""
 def word_to_object(word: str):
     word = word.lower()
 
