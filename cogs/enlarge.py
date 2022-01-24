5cded8d19a59cbcd22e6a4cbfe0e585cefc7d9ee
Achxy <luminosityzzz00@gmail.com>
2022-01-25 02:21:21 +0530

    enlarge command now has usage

--- cogs/enlarge.py
+++ cogs/enlarge.py
@@ -1,6 +1,9 @@
 import discord
 import typing
 from discord.ext import commands
+from database_tools import increment_usage
+
+cmd_type = "cmd_enlarge"
 
 
 class enlarge(commands.Cog):
@@ -9,6 +12,9 @@ class enlarge(commands.Cog):
 
     @commands.command()
     async def enlarge_(self, ctx, *emotes: typing.Union[discord.PartialEmoji, str]):
+
+        successful_additions = 0
+
         for index, i in enumerate(emotes):
 
             if not isinstance(i, discord.PartialEmoji):
@@ -24,7 +30,11 @@ class enlarge(commands.Cog):
             embed.set_footer(
                 text=f"{index + 1} of {len(emotes)} to enlarge {'' if not (index + 1) == len(emotes) else '(over)'}"
             )
+
+            successful_additions += 1
             await ctx.send(embed=embed)
+
+        await increment_usage(self.bot.db, ctx, cmd_type, successful_additions)
 
 
 def setup(bot):
