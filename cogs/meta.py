fd489a2c856a622c34ee737855af76c013630273
Achxy <luminosityzzz00@gmail.com>
2022-01-26 15:42:53 +0530

    setprefix command now has usage

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from database_tools import increment_usage
 
 
 class meta(commands.Cog):
@@ -18,14 +19,20 @@ class meta(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def setprefix(self, ctx, new_prefix):
 
+        cmd_type = "cmd_setprefix"
+
         query = "SELECT prefix FROM guilds WHERE guild_id = $1"
         old_prefix = await self.bot.db.fetch(query, ctx.guild.id)
         old_prefix = old_prefix[0].get("prefix")
 
         query = "UPDATE guilds SET prefix = $1 WHERE guild_id = $2"
         await self.bot.db.execute(query, new_prefix, ctx.guild.id)
-        embed=discord.Embed(title="Successfully changed prefix", description=f"The old prefix used to be **{old_prefix}** now its **{new_prefix}**")
+        embed = discord.Embed(
+            title="Successfully changed prefix",
+            description=f"The old prefix used to be **{old_prefix}** now its **{new_prefix}**",
+        )
         await ctx.send(embed=embed)
+        await increment_usage(self.bot.db, ctx, cmd_type, 1)
 
 
 def setup(bot):
