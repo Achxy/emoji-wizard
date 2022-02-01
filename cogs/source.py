ea79ec6edb9f52f199b7fcd6cd70bf57e96eb65a
Achxy <74144790+Achxy@users.noreply.github.com>
2022-02-02 01:27:00 +0530

    new source command

---
+++ cogs/source.py
@@ -0,0 +1,56 @@
+import discord
+import inspect
+import os
+from discord.ext import commands
+
+
+class source(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def source(self, ctx, *, command: str = None):
+        """
+        Displays my full source code or for a specific command.
+        """
+        source_url = "https://github.com/Achxy/emoji-wizard"
+        branch = "main"
+        if command is None:
+            embed = discord.Embed(
+                title="Emoji-wizard is an open source project!",
+                description=f"You can find it's source code here : **{source_url}**",
+            )
+            await ctx.send(embed=embed)
+            return
+
+        if command == "help":
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+
+        else:
+            obj = self.bot.get_command(command.replace(".", " "))
+            if obj is None:
+                return await ctx.send("Could not find command.")
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith("discord"):
+            # not a built-in command
+            location = os.path.relpath(filename).replace("\\", "/")
+        else:
+            location = module.replace(".", "/") + ".py"
+            source_url = "https://github.com/Rapptz/discord.py"
+            branch = "master"
+
+        final_url = f"<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>"
+        await ctx.send(final_url)
+
+
+def setup(bot):
+    bot.add_cog(source(bot))
