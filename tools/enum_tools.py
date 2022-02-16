641a9ddbee1fdc64da07499581aba81ee07531aa
Achxy <luminosityzzz00@gmail.com>
2022-02-16 15:05:48 +0530

    removed preferences from enums

--- tools/enum_tools.py
+++ tools/enum_tools.py
@@ -6,6 +6,4 @@ __all__ = ("TableType",)
 class TableType(Enum):
     command = "usage"
     rubric = "emoji_rubric"
-    channel_preference = "channel_preferences"
-    command_preference = "command_preferences"
     guilds = "guilds"
