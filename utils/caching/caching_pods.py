e75a3229c3379335bd2658c73c7d3e000ade078a
Achxy <74144790+Achxy@users.noreply.github.com>
2022-03-28 06:35:58 +0530

    fix some whitespace issues

--- utils/caching/caching_pods.py
+++ utils/caching/caching_pods.py
@@ -207,15 +207,15 @@ class CachingPod(MutableMapping[_KT, _VT], EventDispatchers):
                     SELECT
                         column_name as PRIMARYKEYCOLUMN
 
-                    FROM 
-                        INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC 
+                    FROM
+                        INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
 
                     INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
-                        ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' 
-                        AND TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME 
+                        ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY'
+                        AND TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME
                         AND KU.table_name= $1
 
-                    ORDER BY 
+                    ORDER BY
                         KU.TABLE_NAME,
                         KU.ORDINAL_POSITION;
                     """
