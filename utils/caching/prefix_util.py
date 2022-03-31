3098b1ca124d3b4c2a660b04a7cf1e8657b33f2e
Achxy <74144790+Achxy@users.noreply.github.com>
2022-03-31 15:53:12 +0530

    type annotations

--- utils/caching/prefix_util.py
+++ utils/caching/prefix_util.py
@@ -1,14 +1,19 @@
-import os
+from __future__ import annotations
 
-import asyncpg
+from typing import TYPE_CHECKING
+
+from discord import Message
 
 from .cache import BaseCache
+
+if TYPE_CHECKING:
+    from ...main import EmojiBot
 
 _MISSING = object()
 
 
 class PrefixHelper(BaseCache):
-    def __call__(self, bot, message) -> list[str]:
+    def __call__(self, bot: EmojiBot, message: Message) -> list[str]:
 
         return (
             self.get(message.guild.id if message.guild else _MISSING, []) + self.default
