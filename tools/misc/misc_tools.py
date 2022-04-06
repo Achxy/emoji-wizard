124c78ab61bbbf610a2bdc1a6484e6d94b742b5d
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-07 01:56:24 +0530

    dotenv dep

--- tools/misc/misc_tools.py
+++ tools/misc/misc_tools.py
@@ -16,6 +16,9 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 __all__: tuple[str] = ("findenv",)
 
