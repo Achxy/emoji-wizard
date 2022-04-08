52181ad14f7897d92fed6995a6e902ce63f52197
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-08 23:47:44 +0530

    helpers wasn't probably necessary

--- bot/utils/alitecachepg/core/helpers.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
-from asyncpg import Record
-
-
-def textualize(rec: list[Record], join="") -> str:
-    return join.join(map(lambda x: str(x), rec))
