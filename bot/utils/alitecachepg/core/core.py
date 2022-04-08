0402cbfaf6869a57a485145e6694a7a0bf8558a2
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-09 01:08:23 +0530

    docs

--- bot/utils/alitecachepg/core/core.py
+++ bot/utils/alitecachepg/core/core.py
@@ -25,9 +25,13 @@ from queries import Queries
 
 
 class ALiteCache(asyncpg.Pool):
+    """
+    A wrapper around `asyncpg.Pool` that provides a cache for queries.
+    """
+
     __slots__: ClassVar[tuple[str, ...]] = (
-        "con",
-        "cur",
+        "__con",
+        "__cur",
         "__pull_complete",
     )
 
@@ -45,8 +49,8 @@ class ALiteCache(asyncpg.Pool):
         record_class,
         **connect_kwargs,
     ) -> None:
-        self.con = sqlite3.connect(":memory:")
-        self.cur = self.con.cursor()
+        self.__con = sqlite3.connect(":memory:")
+        self.__cur = self.__con.cursor()
         self.__pull_complete = False
         super().__init__(
             *connect_args,
@@ -62,57 +66,97 @@ class ALiteCache(asyncpg.Pool):
             **connect_kwargs,
         )
 
-    async def pull(self):
-        self.con = sqlite3.connect(":memory:")
-        self.cur = self.con.cursor()
+    async def pull(self) -> None:
+        """
+        Pulls the latest data from the database and stores it in sqlite3 memory.
+        """
+        self.__con = sqlite3.connect(":memory:")
+        self.__cur = self.__con.cursor()
 
         await super().execute(Queries.CLONER.value)
         tables = await super().fetch(Queries.GET_SCHEMA_PUBLIC_TABLES.value)
 
-        for t in tables:
-            table_name = t["table_name"]
+        for tble in tables:
+            table_name = tble["table_name"]
             table_creator = await super().fetch(
                 "SELECT * FROM generate_create_table_statement($1);", table_name
             )
-            self.cur.execute(table_creator[0]["generate_create_table_statement"])
+            self.__cur.execute(table_creator[0]["generate_create_table_statement"])
 
-            # Prevent any possible SQL injection
-            # Although this is completely safe
-            # TODO: ...
             remote_contents = await super().fetch(f"SELECT * FROM {table_name}")
             if not remote_contents:
                 continue
             local_query = f"INSERT INTO {table_name} VALUES ({','.join(['?'] * len(remote_contents[0]))});"
-            self.cur.executemany(local_query, remote_contents)
+            self.__cur.executemany(local_query, remote_contents)
 
-        self.con.commit()
+        self.__con.commit()
         self.__pull_complete = True
 
     async def do_pull_if_not_complete(self):
+        """
+        Completes `pull` if it is not complete atleast once.
+        """
         if not self.__pull_complete:
             await self.pull()
 
     async def execute(self, query: str, *args, timeout: float | None = None):
+        """
+        Executes a query on the database and then acts on the cache
+        then commits the changes.
+
+        Args:
+            query (str): The query to execute.
+            timeout (float | None, optional): Time it should take to complete the query. Defaults to None.
+        """
         await self.do_pull_if_not_complete()
         ret = await super().execute(query, *args, timeout=timeout)  # type: ignore
-        self.cur.execute(query, args)
-        self.con.commit()
+        self.__cur.execute(query, args)
+        self.__con.commit()
         return ret
 
     async def executemany(self, command: str, args, *, timeout: float | None = None):
+        """
+        Executes the command the by iterating over each argument and then acts on the cache
+        then commits the changes.
+
+        Args:
+            command (str): The command to execute.
+            args (_type_): The arguments to execute the command with.
+            timeout (float | None, optional): Time it should take to complete the query. Defaults to None.
+        """
         await self.do_pull_if_not_complete()
         ret = await super().executemany(command, args, timeout=timeout)  # type: ignore
-        self.cur.executemany(command, args)
-        self.con.commit()
+        self.__cur.executemany(command, args)
+        self.__con.commit()
         return ret
 
     async def fetch(self, query: str, *args, timeout: float | None = None):
+        """
+        Fetches the value from cache and returns in
+
+        Args:
+            query (str): The query to execute.
+            args (_type_): The arguments to execute the command with.
+
+        Returns:
+            _type_: The value returned from the cache.
+        """
         await self.do_pull_if_not_complete()
-        return self.cur.execute(query, args).fetchall()
+        return self.__cur.execute(query, args).fetchall()
 
     async def fetchrow(self, query: str, *args, timeout: float | None = None):
+        """
+        Fetches the value from cache and returns the first row in the result.
+
+        Args:
+            query (str): The query to execute.
+            timeout (float | None, optional): Time it should take to complete the query. Defaults to None.
+
+        Returns:
+            _type_: The first row returned from the cache.
+        """
         await self.do_pull_if_not_complete()
-        return self.cur.execute(query, args).fetchone()
+        return self.__cur.execute(query, args).fetchone()
 
     def __await__(self):
         yield from super().__await__()
@@ -134,6 +178,10 @@ async def create_caching_pool(
     record_class=protocol.Record,
     **connect_kwargs,
 ) -> ALiteCache:
+    """
+    Instantiates `ALiteCache` with the given parameters.
+    and then awaits it and returns the instance.
+    """
     ret = ALiteCache(
         dsn,
         connection_class=connection_class,
