e8e7b85f85568210debb9fbc088a3dd36d552640
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-18 19:51:49 +0530

    license header files

--- bot/utils/caching/queries/create.sql
+++ bot/utils/caching/queries/create.sql
@@ -1,3 +1,21 @@
+/*
+EmojiWizard is a project licensed under GNU Affero General Public License.
+Copyright (C) 2022-present  Achxy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 -- Create the prefixes table if it doesn't exist already.
 -- Consider not using arrays here because of portability hinderance
 -- and 1NF incoompliancy.
