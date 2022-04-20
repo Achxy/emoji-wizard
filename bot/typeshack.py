b19e20a634c6540932d92bc7d1e0d53e352c22a2
Achxy <74144790+Achxy@users.noreply.github.com>
2022-04-20 22:26:23 +0530

    new typeshack file

---
+++ bot/typeshack.py
@@ -0,0 +1,31 @@
+"""
+EmojiWizard is a project licensed under GNU Affero General Public License.
+Copyright (C) 2022-present  Achxy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from typing import TYPE_CHECKING, Callable, Iterable, TypeAlias, TypeVar
+
+from discord import Message
+
+if TYPE_CHECKING:
+    from core import EmojiBot
+    from typing_extensions import Unpack
+
+    PassIntoBase: TypeAlias = Callable[
+        [Unpack[tuple[str, ...]]], Callable[[EmojiBot, Message], Iterable[str]]
+    ]
+else:
+    EmojiBot = TypeVar("EmojiBot")
+    PassIntoBase = TypeVar("PassIntoBase", bound=Callable)
